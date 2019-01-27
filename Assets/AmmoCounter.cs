c0c4d6a21dc158c33e0a52f4f5ca3287bba1c778
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2019-01-27 14:57:27 +0000

    Tweaks and added ammo counter

---
+++ Assets/AmmoCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoCounter : MonoBehaviour {
+
+	[SerializeField] UFO parent;
+	private Text myText;
+
+	void Awake(){
+		myText = GetComponent<Text> ();
+	}
+
+	void Update(){
+		myText.text = parent.passengers.ToString ();
+	}
+}
